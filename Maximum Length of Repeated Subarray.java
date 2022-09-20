a5b92cb1c3cbba2fa246fa417f5132dfba3eced6
gk34961 <51943620+gk34961@users.noreply.github.com>
2022-09-20 20:47:03 +0530

    Create Maximum Length of Repeated Subarray.java
    Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
    Output: 3
    Explanation: The repeated subarray with maximum length is [3,2,1].
    Example 2:
    
    Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
    Output: 5
     
    
    Constraints:
    
    1 <= nums1.length, nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 100

---
+++ Maximum Length of Repeated Subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int dp[][]=new int[nums1.length+1][nums2.length+1];
+        dp[0][0]=0;
+        int max=0;
+        for(int i=1;i<=nums1.length;i++){
+            for(int j=1;j<=nums2.length;j++){
+                if(nums1[i-1]==nums2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=0;
+                max=Math.max(max,dp[i][j]);
+            }
+        }
+       
+        
+    return max;
+    }
+}
