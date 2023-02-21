9cdf2c408ee6c035f511114ffd17bb80a35ae586
gk34961 <51943620+gk34961@users.noreply.github.com>
2023-02-21 23:51:53 +0530

    Update Largest BST.java

--- Largest BST.java
+++ Largest BST.java
@@ -34,3 +34,25 @@ class Solution{
             return new Tree(false,Math.max(left.size,right.size),-1,-1);
     }
 }
+
+=======================================================================================================================================================================
+class Solution{
+    // Return the size of the largest sub-tree which is also a BST
+    static int largestBst(Node root)
+    {
+        // Write your code here
+        if(root==null) return 0;
+        if(isBst(root,Integer.MIN_VALUE,Integer.MAX_VALUE))
+        return size(root);
+        return Math.max(largestBst(root.left),largestBst(root.right));
+    }
+    static int size(Node node){
+        if(node==null) return 0;
+        return 1+size(node.left)+size(node.right);
+    }
+    static boolean isBst(Node root,int min,int max){
+        if(root==null) return true;
+        if(root.data<min||root.data>max) return false;
+        return isBst(root.left,min,root.data-1) && isBst(root.right,root.data+1,max);
+    }
+}
